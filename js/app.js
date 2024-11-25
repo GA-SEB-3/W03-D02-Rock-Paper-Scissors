cace1c4d31b0405c3a8a8943638e211869bc2ec1
omarakamal <omaraakamal@gmail.com>
2024-11-25 19:55:30 +0300

    updated code

--- js/app.js
+++ js/app.js
@@ -22,9 +22,31 @@ const resultDisplayEl = document.querySelector("#result-display")
 
 const buttonChoicesElement = document.querySelectorAll(".button-choice")
 
+const compare = () =>{
+    // TIE Condition
+    if(playerChoice === computerChoice){
+        msg = "You tied!"
+    }
+    // WIN Conditions
+    else if(playerChoice=== choices[0] && computerChoice === choices[2]){
+        msg = "Congrats! You Win"
+    }
+    // WIN Conditions
+    else if(playerChoice === choices[1] && computerChoice === choices[0]){
+        msg = "Congrats! You Win"
+    }
+    // WIN Conditions
+    else if(playerChoice === choices[2] && computerChoice === choices[1]){
+        msg = "Congrats! You Win"
+    }
+
+    else{
+        msg = "You Lose! Try Again"
+    }
+}
+
 
 // console.log(Math.ceil(1.5))
-console.log()
 const play = (event)=>{
         playerChoice = event.target.id
         const randomIndex = Math.floor(Math.random() * 3)
